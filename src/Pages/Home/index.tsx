b85f022771a5f969aeb302cec4e24584fea1bdb2
dankwx <danielkondlatsch.p@gmail.com>
2024-01-10 01:08:17 -0300

    feature: finished navbar

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -6,6 +6,12 @@ export default function Home() {
   return (
     <div className={styles.mainWrapper}>
       <Navbar />
+      <div className={styles.home}>
+        <h1>Daniel</h1>
+        <h1>Kondlatsch</h1>
+        <h1>Web</h1>
+        <h1>Developer</h1>
+      </div>
     </div>
   );
 }
